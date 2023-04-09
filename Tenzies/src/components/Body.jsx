9040bd5ff81d2d16b2f14c0fad36bc88fbc7ce33
J-Skarbek <john@snarkmediaagency.com>
2023-04-09 16:01:11 -0500

    establish state for the values of dice within the body component and test to ensure value is passed to each die component

--- Tenzies/src/components/Body.jsx
+++ Tenzies/src/components/Body.jsx
@@ -3,6 +3,8 @@ import Die from "./Die";
 
 function Body() {
 
+  const [value, setValue] = React.useState(4);
+
   return (
     <div className="body-container col-start-6 col-span-5 row-start-2 row-span-3 bg-dark-purple p-8">
       <div className="background-overlay bg-bright-yellow p-4 h-full flex flex-col justify-evenly content-center text-center">
@@ -10,14 +12,18 @@ function Body() {
         <p className="text-xl">Roll until all dice are the same. 
         Click each die to freeze it at its current value between rolls.</p>
         <div className="dice-container-1 flex space-evenly items-center justify-center flex-wrap gap-3">
-          <Die />
+          <Die
+            value={value} 
+          />
           <Die />
           <Die />
           <Die />
           <Die />
         </div>
         <div className="dice-container-2 flex space-evenly items-center justify-center flex-wrap gap-3">
-          <Die />
+          <Die
+            value={value} 
+          />
           <Die />
           <Die />
           <Die />
--- Tenzies/src/components/Die.jsx
+++ Tenzies/src/components/Die.jsx
@@ -4,7 +4,8 @@ function Die(props) {
 
   return (
     <div className="die bg-light-purple px-4 py-2 rounded-lg drop-shadow-xl">
-      <p className="value text-dark-purple font-bold text-2xl">6</p>
+      <p className="value text-dark-purple font-bold text-2xl">{props.value ? props.value : '6'}</p>
+      {/* <p className="value text-dark-purple font-bold text-2xl">6</p> */}
     </div>
   )
 }
