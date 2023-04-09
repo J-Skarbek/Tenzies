31e3c1d1426257b2e8778a84616e22357a22fa87
J-Skarbek <john@snarkmediaagency.com>
2023-04-09 12:01:05 -0500

    Add fonts/colors to tailwind and css files

--- Tenzies/tailwind.config.js
+++ Tenzies/tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'quicksand': ['Quicksand', 'sans-serif'],
+    },
+    colors: {
+      'bright-yellow': '#FAF535',
+      'dark-purple': '#4500AD',
+      'mid-purple': '#6907FA',
+      'light-purple': '#7a21ff',
+      'dark-blue': '#1D00AD',
+      'mid-blue': '#3712FA',
+      'light-blue': '#4F2BFF',
+      'off-white-yellow': '#F7F3DF',
+      'off-white-blue': '#DDDEFE',
+    },
     extend: {},
   },
   plugins: [],
